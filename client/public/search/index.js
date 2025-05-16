f56b89d65af747b873df33f8bae3fe70eb07fb54
Gael Melo <104900699+gmmelo@users.noreply.github.com>
2025-05-16 07:39:42 -0700

    Add working search page

--- client/public/search/index.js
+++ client/public/search/index.js
@@ -1,5 +1,22 @@
 "use strict";
-function displaySearch() {
+function get_class_html(class_json) {
+    let inner_content;
+    try {
+        inner_content = `<p>Class: ${class_json["SUBJECT"]} ${class_json["CATALOGNBR"]}</p>`
+            + `<p>Title: ${class_json["TITLE"]}</p>`
+            + `<p>Instructor: ${class_json["INSTRUCTORSLIST"] == null ? "Not yet defined" : class_json["INSTRUCTORSLIST"][0]}</p>`
+            + `<p>Seats taken: ${class_json["ENRLTOT"]}/${class_json["ENRLCAP"]}</p>`
+            + `<p>#${class_json["CLASSNBR"]}</p>`;
+    }
+    catch (err) {
+        console.log(`Error creating class listing: ${err}`);
+        console.log(class_json);
+        inner_content = "<p>Failed displaying this class!</p>";
+    }
+    const full_string = `<div class="class-listing">${inner_content}</div>`;
+    return full_string;
+}
+async function displaySearch() {
     try {
         const search = window.location.search;
         const params = new URLSearchParams(search);
@@ -8,6 +25,14 @@ function displaySearch() {
         document.title = `Search: ${subject} ${number}`;
         const search_title = document.getElementById("search-title") ?? document.createElement("h1");
         search_title.innerHTML += `${subject} ${number}`;
+        const url = `https://eadvs-cscc-catalog-api.apps.asu.edu/catalog-microservices/api/v1/search/classes?&refine=Y&campusOrOnlineSelection=A&catalogNbr=${number}&honors=F&promod=F&searchType=all&subject=${subject}&term=2257`;
+        const response = await (await fetch(url, { headers: { "authorization": "Bearer null" } })).json();
+        const search_results = document.getElementById("search-results") ?? document.createElement("p");
+        const class_list = response["classes"];
+        search_results.innerHTML = "";
+        for (let i = 0; i < class_list.length; i++) {
+            search_results.innerHTML += get_class_html(class_list[i]["CLAS"]);
+        }
     }
     catch (err) {
         console.log(`Couldn't display search results! ${err}`);
--- client/src/search/index.ts
+++ client/src/search/index.ts
@@ -1,4 +1,39 @@
-function displaySearch(): void {
+type Class = {
+    "CLAS":
+        {
+            "CLASSNBR": string,
+            "SUBJECT": string,
+            "CATALOGNBR": string,
+            "TITLE": string,
+            "INSTRUCTORSLIST": Array<string> | null,
+            "ENRLTOT": string,
+            "ENRLCAP": string
+        }
+}
+
+type SearchResults = {
+    "classes": Array<Class>;
+}
+
+function get_class_html(class_json: Class["CLAS"]): string {
+    let inner_content: string;
+    try {
+        inner_content = `<p>Class: ${class_json["SUBJECT"]} ${class_json["CATALOGNBR"]}</p>`
+            + `<p>Title: ${class_json["TITLE"]}</p>`
+            + `<p>Instructor: ${class_json["INSTRUCTORSLIST"] == null ? "Not yet defined" : class_json["INSTRUCTORSLIST"][0]}</p>`
+            + `<p>Seats taken: ${class_json["ENRLTOT"]}/${class_json["ENRLCAP"]}</p>`
+            + `<p>#${class_json["CLASSNBR"]}</p>`;
+    }
+    catch(err) {
+        console.log(`Error creating class listing: ${err}`);
+        console.log(class_json);
+        inner_content = "<p>Failed displaying this class!</p>";
+    }
+    const full_string: string = `<div class="class-listing">${inner_content}</div>`; 
+    return full_string;
+}
+
+async function displaySearch(): Promise<void> {
     try {
         const search: string = window.location.search;
         const params: URLSearchParams = new URLSearchParams(search);
@@ -9,9 +44,20 @@ function displaySearch(): void {
         document.title = `Search: ${subject} ${number}`
         const search_title: HTMLElement = document.getElementById("search-title") ?? document.createElement("h1");
         search_title.innerHTML += `${subject} ${number}`;
+
+        const url: string = `https://eadvs-cscc-catalog-api.apps.asu.edu/catalog-microservices/api/v1/search/classes?&refine=Y&campusOrOnlineSelection=A&catalogNbr=${number}&honors=F&promod=F&searchType=all&subject=${subject}&term=2257`;
+        const response: SearchResults = await (await fetch(url, {headers: {"authorization": "Bearer null"}})).json();
+
+        const search_results: HTMLElement = document.getElementById("search-results") ?? document.createElement("p");
+        const class_list: Array<Class> = response["classes"];
+
+        search_results.innerHTML = "";
+        for (let i: number = 0; i < class_list.length; i++) {
+            search_results.innerHTML += get_class_html(class_list[i]["CLAS"]);
+        }
     }
     catch(err) {
-        console.log(`Couldn't display search results! ${err}`)
+        console.log(`Couldn't display search results! ${err}`);
     }
 }
 
